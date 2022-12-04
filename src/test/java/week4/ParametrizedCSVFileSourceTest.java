ec03d48232e88f598b8be33c322a523db74cea65
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-04 22:09:15 +0300

    week 4 forth push.

---
+++ src/test/java/week4/ParametrizedCSVFileSourceTest.java
@@ -0,0 +1,10 @@
+package week4;
+
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+public class ParametrizedCSVFileSourceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "")
+}
--- src/test/java/week4/ParametrizedCSVSourceTest.java
+++ src/test/java/week4/ParametrizedCSVSourceTest.java
@@ -1,10 +1,15 @@
 package week4;
 
+import io.restassured.http.*;
+import io.restassured.response.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
+import utilities.*;
 
-public class ParametrizedCSVSourceTest {
+import static io.restassured.RestAssured.given;
+
+public class ParametrizedCSVSourceTest extends SpartanTestBase {
 
     @ParameterizedTest
     @CsvSource({"1,2,1",
@@ -13,6 +18,23 @@ public class ParametrizedCSVSourceTest {
     public void test1(int a, int b, int c){
         Assertions.assertEquals(Math.pow(a,b), c);
     }
+
+
+    @ParameterizedTest
+    @CsvSource({"20,Lothario",
+            "21, Mark",
+            "22, Koenraad"})
+    public void test2(int id, String name){
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .pathParam("id", id)
+                .when().get("/api/spartans/{id}");
+
+        Assertions.assertEquals(name,response.path("name"));
+
+    }
+
+
 
 
 
