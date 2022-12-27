dc13026741c2136da2d8354b6e01461d44e2ee71
fatihcam46 <fatih.cam.bahar@gmail.com>
2022-12-27 20:47:05 +0000

    bootcamp review -RequestAndResponse

--- src/test/java/BootCampReview/RequestAndResponse.java
+++ src/test/java/BootCampReview/RequestAndResponse.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-
+//1
 public class RequestAndResponse {
 
     @BeforeAll
@@ -18,79 +18,82 @@ public class RequestAndResponse {
         baseURI = "http://3.87.65.105:8000";
     };
 
-
-
     @Test
     public void test1(){
 
         String base_uri ="http://3.87.65.105:8000/api/spartans";
 
-        Response response = given().accept(ContentType.JSON)
+        Response response = given().accept(ContentType.JSON)   //1st we need object.create it
                 .when().get(base_uri);
-
-//        response.prettyPrint();
-        System.out.println(response.statusCode());
-        System.out.println(response.contentType());
-        System.out.println(response.headers().toString());
-        System.out.println(response.headers().hasHeaderWithName("Keep-Alive"));
-        System.out.println(response.header("Transfer-Encoding"));
+//        response.prettyPrint();  if I run ,I can see all spartan
+        System.out.println(response.statusCode());//200
+        System.out.println(response.contentType());//application/json
+        System.out.println(response.headers().toString());//all headers
+        System.out.println(response.headers().hasHeaderWithName("Keep-Alive"));//true
+        System.out.println(response.header("Transfer-Encoding"));//chunked
     }
-
+//-----------------------------------------------------------------------------
     @Test
     public void test2(){
-
+//path param method
         baseURI = "http://3.87.65.105:8000";
 
 //        Response response = given().accept(ContentType.JSON)
 //                .when().get("/api/spartans/20");
 
-        // providing id number using path param method
+     // providing id number using path param method
         Response response = given().accept(ContentType.JSON)
-                        .and().pathParam("id",20)
+                        .and().pathParam("id",20)//key and value  : path param
                         .when().get("/api/spartans/{id}");
 
         Assertions.assertEquals(200,response.statusCode());
         response.prettyPrint();
-
-        System.out.println(response.body().asString().contains("Lothario"));
+/*{
+    "id": 20,
+    "name": "Lothario",
+    "gender": "Male",
+    "phone": 7551551687
+}                        */
+        System.out.println(response.body().asString().contains("Lothario"));//true
 
         //path method
-        System.out.println(response.path("name").toString());
+        System.out.println(response.path("name").toString());//Lothario
+        //our user story is saying: check name is it Lothario?  so we need  Assertions
         Assertions.assertEquals("Male",response.path("gender"));
     }
-
+//---------------------------------------------------------------------------
     @Test
     public void test3(){
-        // qeury param method
+// query param method
         Response response = given().accept(ContentType.JSON)
                 .and().queryParam("gender","Male")
                 .when().get("/api/spartans/search");
 
-        System.out.println(response.statusCode());
-        response.prettyPrint();
-
-        System.out.println(response.path("content[0].name").toString());
+        System.out.println(response.statusCode());//200
+        response.prettyPrint();//all spartans
+                                             //we need to add  content[index number]
+        System.out.println(response.path("content[0].name").toString());//Lavonne
     }
-
+//--------------------------------------------------------------------
     @Test
     public void test4(){
         Response response = given().accept(ContentType.JSON)
                 .and().queryParam("nameContains","da")
                 .when().get("/api/spartans/search");
-
-//        response.prettyPrint();
+//        response.prettyPrint();//every name include da
+//jsonPath method
         JsonPath jsonPath = response.jsonPath();
-        System.out.println(jsonPath.getInt("totalElement"));
+        System.out.println(jsonPath.getInt("totalElement"));//9
 
         // json path get list
-        List<Map<String, Object>> conents = jsonPath.getList("content");
-        System.out.println(conents.get(1).toString());
+        List<Map<String, Object>> contents = jsonPath.getList("content");
+        System.out.println(contents.get(1).toString());//{id=68, name=Adda, gender=Female, phone=5361981152}
 
-        // json path iterate method
+        // json path iterate method                                 iterate(it) all id number:68
         List<String> name = jsonPath.getList("content.findAll {it.id==68}.name");
-        System.out.println("name = " + name);
+        System.out.println("name = " + name);//name = [Adda]
     }
-
+//------------------------------------------------------------------------
     // provide params in a map
     @Test
     public void test5(){
@@ -100,8 +103,8 @@ public class RequestAndResponse {
 //                .when().get("/api/spartans/search");
 
         Map<String, Object> params = new HashMap<>();
-        params.put("nameContains","b");
-        params.put("gender","Female");
+        params.put("nameContains","b");  //contains b
+        params.put("gender","Female");   // gender female
 
         Response response = given().accept(ContentType.JSON)
                 .and().queryParams(params)
